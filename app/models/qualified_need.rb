62a399762aaef7f11e87d47f439a7b8dd25f3510
Joe Hainline <joe.hainline@asynchrony.com>
2016-10-22 14:06:44 -0500

    fixed typo

--- app/models/qualified_need.rb
+++ app/models/qualified_need.rb
@@ -1,7 +1,7 @@
 class QualifiedNeed < ActiveRecord::Base
 
   acts_as_mappable :lat_column_name => :lat,
-                   :lng_column_name => :lng,
+                   :lng_column_name => :lng
                    # :auto_geocode=>{:field=>:address, :error_message=>'Could not geocode address'}
 
   def address
