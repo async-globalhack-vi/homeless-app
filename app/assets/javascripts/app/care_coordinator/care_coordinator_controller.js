131c6a1ba8abaec7a4cc4184d26491a83ed3ed87
Matt Todd <matt.c.todd@gmail.com>
2016-10-22 06:43:17 -0500

    added wire-in to backend

--- app/assets/javascripts/app/care_coordinator/care_coordinator_controller.js
+++ app/assets/javascripts/app/care_coordinator/care_coordinator_controller.js
@@ -1,13 +1,19 @@
 (function() {
 
-  angular.module('homeless').controller('careCoordinatorController', ['$scope', careCoordinatorController]);
+  angular.module('homeless').controller('careCoordinatorController', ['$scope', '$http', careCoordinatorController]);
 
-  function careCoordinatorController($scope) {
+  function careCoordinatorController($scope, $http) {
     $scope.showForm = false;
 
     $scope.addQualifiedNeed = function() {
       $scope.form = {};
       $scope.showForm = true;
     };
+
+    $scope.on('refresh-qualified-needs', function() {
+      $http.get('/qualified_need').then(function(needs) {
+        $scope.qualifiedNeeds = needs;
+      });
+    });
   }
 })();
