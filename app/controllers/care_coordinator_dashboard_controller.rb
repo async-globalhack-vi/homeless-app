b71adfb9eaa82845393f2422a8c21aa0e2791732
Joe Hainline <joe.hainline@asynchrony.com>
2016-10-23 01:51:18 -0500

    in work charts stuff

--- app/controllers/care_coordinator_dashboard_controller.rb
+++ app/controllers/care_coordinator_dashboard_controller.rb
@@ -2,9 +2,9 @@ class CareCoordinatorDashboardController < ApplicationController
   before_filter :ensure_user_is_care_coordinator
 
   def index
-    # @all_assistance_providers = AssistanceProvider.all
-    # @all_events = Event.all
-    # @all_qualified_needs = QualifiedNeed.all
+    @all_assistance_providers = AssistanceProvider.all
+    @all_events = Event.all
+    @all_qualified_needs = QualifiedNeed.all
   end
 end
 
